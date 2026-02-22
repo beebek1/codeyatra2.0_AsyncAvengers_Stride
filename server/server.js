cb40ed1d715335c8f4a0fa8e16107effbf7f1b29
beebek1 <beebek990@gmail.com>
2026-02-22 15:24:27 +0545

    implement career model, route and controller

---
+++ server/server.js
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { sequelize, connectDB } from "./src/db/db.js";
+import rootRouter from './src/modules/main.router.js';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+// Mount Routes
+app.use("/api", rootRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Home Page" });
+});
+
+const startServer = async () => {
+  const PORT = process.env.PORT || 3000;
+  try {
+    await connectDB();
+
+    // SYNC LOGIC EXPLAINED BELOW
+    await sequelize.sync({ alter: true });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+startServer();
