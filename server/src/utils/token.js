a993fe33820205128526df3b4cd21a341f8d74f8
ani5hpdl <178013051+ani5hpdl@users.noreply.github.com>
2026-02-22 14:53:09 +0545

    Added File for generating token for logged in user

---
+++ server/src/utils/token.js
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (userId) => {
+    const payload = {userId};
+    const token = jwt.sign(payload,process.env.JWT_SECRET,{
+        expiresIn : "7d"
+    });
+
+    return token;
+}
+
+export {generateToken};
