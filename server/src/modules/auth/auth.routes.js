79e0d83a36a7ac405d740416bcb14e2f8ba367e0
BiseshMaharjan10 <biseshmhrz01@gmail.com>
2026-02-23 01:09:15 +0545

    aded getMe function and authGuad and fixed add interest

--- server/src/modules/auth/auth.routes.js
+++ server/src/modules/auth/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { registerUser, login } from "./auth.controller.js";
+import { registerUser, login , getMe} from "./auth.controller.js";
+import authGuard from "../../middleware/authGuard.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
+router.get("/getme", authGuard, getMe);
 
 export default router;
